e538bb81b1db6f778946458dc2ff7afa95f8bdf7
Manu Sridharan <msridhar@gmail.com>
2026-08-22 13:21:40 -0700

    Update signature of Map.computeIfAbsent

--- src/java.base/share/classes/java/util/Map.java
+++ src/java.base/share/classes/java/util/Map.java
@@ -1081,8 +1081,8 @@ public interface Map<K extends @Nullable Object, V extends @Nullable Object> {
      *         ({@linkplain Collection##optional-restrictions optional})
      * @since 1.8
      */
-    default V computeIfAbsent(K key,
-            Function<? super K, ? extends V> mappingFunction) {
+    default @Nullable V computeIfAbsent(K key,
+            Function<? super K, ? extends @Nullable V> mappingFunction) {
         Objects.requireNonNull(mappingFunction);
         V v;
         if ((v = get(key)) == null) {
